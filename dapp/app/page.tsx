628abd2b4e101f2606c496d624714d7de9a24a34
jreevesy <john.reeves.yecla@adamson.edu.ph>
2024-03-04 00:48:20 +0800

    add some

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -5,18 +5,12 @@ import { useEffect, useState } from "react";
 import { getContract } from "../config";
 
 export default function Home() {
-  // State for wallet connection
   const [walletKey, setWalletKey] = useState("");
-
-  // State for minting
   const [mintingAmount, setMintingAmount] = useState<number>();
   const [submitted, setSubmitted] = useState(false);
   const [transactionHash, setTransactionHash] = useState("");
-
-  // State for staking
   const [stakingAmount, setStakingAmount] = useState<number>();
 
-  // Connection to the wallet
   const connectWallet = async () => {
     const { ethereum } = window as any;
     const accounts = await ethereum.request({
@@ -25,7 +19,6 @@ export default function Home() {
     setWalletKey(accounts[0]);
   };
 
-  // Minting function
   const mintCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -43,7 +36,6 @@ export default function Home() {
     }
   };
 
-  // Mint amount change handler
   const mintAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
@@ -53,7 +45,6 @@ export default function Home() {
     }
   };
 
-  // Staking function
   const stakeCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -71,7 +62,6 @@ export default function Home() {
     }
   };
 
-  // Staking amount change handler
   const stakeAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     if (!isNaN(Number(inputValue))) {
@@ -81,7 +71,6 @@ export default function Home() {
     }
   };
 
-  // Withdraw function
   const withdrawCoin = async () => {
     const { ethereum } = window as any;
     const provider = new BrowserProvider(ethereum);
@@ -93,8 +82,6 @@ export default function Home() {
       await tx.wait();
       setSubmitted(true);
       setTransactionHash(tx.hash);
-
-      // Show alert when withdrawal is successful
       window.alert("Withdraw Done");
     } catch (e: any) {
       const decodedError = contract.interface.parseError(e.data);
@@ -103,91 +90,87 @@ export default function Home() {
   };
 
   return (
-    <main style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      minHeight: '100vh',
-      backgroundImage: `url('https://wallpapercave.com/wp/wp8865987.jpg')`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}>
-      <div style={{
-        border: '2px solid rgba(255, 255, 255, 0.2)',
-        backdropFilter: 'blur(20px)',
-        boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-        padding: '80px',
-        margin: '20px',
-        textAlign: 'center',
-        background: 'transparent'
-      }}>
+    <main style={mainStyle}>
+      <div style={containerStyle}>
         <form>
-          <label> Contract Id: 0x63CdD3987B09917A140a8C4597029419f3Ff7Bc5</label><br></br>
+          <label> Contract Id: 0x63CdD3987B09917A140a8C4597029419f3Ff7Bc5</label><br />
         </form>
+
         <button
-          onClick={() => { connectWallet(); }}
-          className="p-3 bg-violet-900 text-black rounded transition duration-300 hover:ring-4 hover:ring-violet-700"
-        >
+        onClick={connectWallet}
+        className="p-3 bg-violet-900 text-black transition duration-300 hover:ring-4 hover:ring-violet-700"
+        style={{ borderRadius: '0', marginRight: '10px' }}>
           {walletKey !== "" ? walletKey : " Connect wallet"}
-        </button>
-
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <br></br>
-          <form>
-            <label> Mint Amount</label><br></br>
-          </form>
-          <input
-            type="text"
-            value={mintingAmount}
-            onChange={(e) => mintAmountChange(e)}
-            style={{ color: "black" }}
-            placeholder="Amount of Mint Tokens"
-          />
-          <button
-            onClick={() => mintCoin()}
-            className="p-3 bg-violet-900 text-black rounded transition duration-300 hover:ring-4 hover:ring-violet-700"
-            style={{ marginTop: '10px' }}
-          >
-            {"Mint"}
           </button>
-        </div>
-        <br></br>
 
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <form>
-            <label> Stake Amount</label><br></br>
-          </form>
-          <input
-            type="text"
-            value={stakingAmount}
-            onChange={(e) => stakeAmountChange(e)}
-            style={{ color: "Black" }}
-            placeholder="Amount of Stake Tokens"
-          />
-          <button
-            onClick={stakeCoin}
-            className="p-3 bg-violet-900 text-black rounded transition duration-300 hover:ring-4 hover:ring-violet-700"
-            style={{ marginTop: '10px' }}
-          >
-            {"Stake"}
-          </button>
-        </div>
+        {renderForm("Mint Amount", mintingAmount, mintAmountChange, mintCoin)}
 
-        <br></br>
+        {renderForm("Stake Amount", stakingAmount, stakeAmountChange, stakeCoin)}
 
         <div>
           <div>
-            <button
-              onClick={withdrawCoin}
-              className="p-3 bg-violet-900 text-black rounded transition duration-300 hover:ring-4 hover:ring-violet-700"
-            >
-              {"Withdraw"}
-            </button>
+          <button
+          onClick={withdrawCoin}
+          className="p-3 bg-violet-900 text-black transition duration-300 hover:ring-4 hover:ring-violet-700"
+          style={{ borderRadius: '0', marginTop: '10px' }}> 
+          {"Withdraw"}</button>
           </div>
         </div>
-
       </div>
     </main>
   );
 }
+
+function renderForm(labelText, value, onChange, onClick) {
+  return (
+    <div style={formContainerStyle}>
+      <form>
+        {/* <label>{labelText}</label><br /> Remove this line */}
+      </form>
+
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e)}
+        style={{ color: "black" }}
+        placeholder={` ${labelText}`}
+      />
+      
+      <button
+        onClick={onClick}
+        className="p-3 bg-violet-900 text-black transition duration-300 hover:ring-4 hover:ring-violet-700"
+        style={{ borderRadius: '0', marginTop: '10px' }}
+      >
+        {labelText}
+      </button>
+    </div>
+  );
+}
+
+const mainStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  backgroundImage: `url('https://wallpapercave.com/wp/wp8865987.jpg')`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+};
+
+const containerStyle = {
+  border: '2px solid rgba(255, 255, 255, 0.2)',
+  backdropFilter: 'blur(20px)',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+  padding: '80px',
+  margin: '20px',
+  textAlign: 'center',
+  background: 'transparent',
+};
+
+const formContainerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '20px',
+};
